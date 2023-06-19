9a4b6db73b16866bb0ab7ee032bf435427b44bbc
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-06-19 11:12:46 +0100

    Updates to style. (too noisy now)

--- css/clay.hs
+++ css/clay.hs
@@ -2,31 +2,36 @@
     OverloadedStrings,
     BlockArguments
   #-}
+import Prelude hiding ((**))
 import Data.Text (Text)
 import qualified Data.Text.Lazy.IO as T
 import Clay
 
+link, link_active :: Color
+link = "#7FB4CA"
+link_active = "#E82424"
 
 -- | Generate CSS for a specific tag type from name and color
 tag :: Text -> Color -> Css
-tag name col = do
+tag name col = ".tags" ? do
   let tagname = element (".tag-" <> name)
   tagname ? do
     important (color col)
     before &
       background col
-    a # ":active" <?
+    a <?
       important (color col)
-
+    -- a # ":active" <?
+    --   important (color link_active)
 
 stylesheet :: Css
 stylesheet = do
   tag "haskell" "#8f4e8b"
-  tag "beginner" "#FFE5B4"
   tag "vim" "#007f00"
-  tag "frp" orange
-  tag "graphics" red
-  tag "game-engine" black
+  tag "beginner" "#98BB6C"
+  tag "frp" "#FF9E3B"
+  tag "graphics" "#E82424"
+  tag "game-engine" "#16161D"
 
 
 -- Output the compact version
