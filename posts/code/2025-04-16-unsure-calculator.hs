eb13cbcc255302733953b4a819e6b88bf10e1877
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-04-17 22:51:08 +0100

    Almost working unsure calculator

--- posts/code/2025-04-16-unsure-calculator.hs
+++ posts/code/2025-04-16-unsure-calculator.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, ViewPatterns #-}
 import Control.Applicative
 import Text.Printf
 import qualified Data.Map as M
+import Data.Foldable (minimumBy, maximumBy)
+import Data.Function (on)
 import Data.Maybe
+import Data.List (transpose)
 
 newtype Dist a = Dist { unpackDist :: [(a, Double)] }
   deriving Functor
@@ -12,13 +15,18 @@ instance Applicative Dist where
   (Dist fs) <*> (Dist xs) = Dist $ do
     (x, px) <- xs
     (f, pf) <- fs
-    return (f x, px * pf)
+    let !fr = f x
+    let !pr = px * pf
+    return (fr, pr)
+  -- {-# INLINE (<*>) #-}
 
 instance Monad Dist where
   (Dist xs) >>= f = Dist $ do
     (x, p) <- xs
     (y, p') <- unpackDist (f x)
-    return (y, p * p')
+    let !pr = p * p'
+    return (y, pr)
+  -- {-# INLINE (>>=) #-}
 
 squishD (Dist xs) = Dist $ M.toList $ M.fromListWith (+) xs
 sumP = sum . map snd
@@ -32,24 +40,7 @@ instance (Ord a, Show a, PrintfArg a) => Show (Dist a) where
       padded elem = replicate (maxElemLen - (length . show) elem) ' ' ++ show elem
       maxElemLen = maximum $ map (length . show . fst) (unpackDist d)
 
--- -- put in 20 buckets
-buckets :: Dist Double -> Dist Double
-buckets (Dist [])   = Dist []
-buckets d@(Dist xs) = Dist $ catMaybes $ fmap interval xs where
-  low  = fst $ head xs
-  high = fst $ last xs
-  step = (high - low)/20
-  steps = [low, low+step .. high]
-  interval (x, p)
-    = case takeWhile (\b -> b <= x) steps of
-        [] -> Nothing
-        (i:_) -> Just (i, p)
-
-
 uniform xs = Dist . normP $ zip xs (repeat 1)
-coin f a b
-  | f < 0 || f > 1 = error "f must be between 0 and 1"
-  | otherwise      = Dist [(a, f), (b, 1 - f)]
 
 data Expr
   = Num Double | Add Expr Expr | Mul Expr Expr
@@ -81,7 +72,7 @@ instance Floating Expr where
   cos = Cos
 
 instance Show Expr where
-  show = show . buckets . eval
+  show = show . collapseIntervals 21 . eval
 
 eval :: Expr -> Dist Double
 eval = \case
@@ -100,20 +91,90 @@ eval = \case
     a <- eval e1
     b <- eval e2
 
+    -- Equations:
     -- μ - 2σ = a
     -- μ + 2σ = b
-    -- μ = b - 2σ = (b + a) / 2
-    -- σ = (μ - a)/2
-    -- σ = (b - 2σ - a)/2
-    -- σ = b/2 - σ - a/2
+    -- Solutions:
     -- σ  = (b - a)/4
     -- μ = b - (b - a)/2 = b - b/2 + a/2 = b/2 + a/2 = (b + a)/2
 
     let
-      samples = [a, a+0.5 .. b]
       mean = (a + b) / 2
       std_dev = (b - a) / 4
-      normal_f x = (1/(std_dev*sqrt(2*pi))) * exp ((-1/2)*(((x-mean)/std_dev)**2))
 
-    Dist $ map (\x -> (x, normal_f x)) samples
+      -- confidence_interval_upper = mean + 1.96*(std_dev/sqrt(fromIntegral $ length samples))
+      -- confidence_interval_lower = mean - 1.96*(std_dev/sqrt(fromIntegral $ length samples))
 
+    collapseIntervals 22 $ normal mean std_dev
+
+
+-- | Draw from a normal distribution
+normal :: Double {-^ Mean -} -> Double {-^ Std dev -} -> Dist Double
+-- generate a handful of discrete samples from this distribution using the Box-Muller transform
+normal mean std_dev = Dist $ normP $
+  filter (not . isInfinite . fst) $
+    map (\z -> (z*std_dev + mean, 1)) $ boxMullers haltonSamples
+  -- "probability=1" for every sample because samples are already distributed
+  -- according to the probability function (s.t. grouping them into intervals
+  -- will already yield higher probability for the intervals near the mean)
+
+intervals :: [Double] -- ^ Each double is a box where all floats less than this number fall, in order (list must be sorted low-to-high).
+          -> Dist Double -- ^ A distribution with arbitrary doubles
+          -> Dist Double -- ^ A distribution where all doubles fall into the given discrete categories
+intervals boxes dist = squishD $ do
+  s <- dist
+  return $ fst $ minimumBy (compare `on` snd) $
+    map (\box -> (box, abs (box - s))) $ filter (not . isInfinite) boxes
+
+-- | Simplify samples into N intervals
+collapseIntervals :: Int -> Dist Double -> Dist Double
+collapseIntervals n d@(Dist xs) =
+  let (low,_)  = minimumBy (compare `on` fst) xs
+      (high,_) = maximumBy (compare `on` fst) xs
+      step = (high - low) / fromIntegral n
+   in intervals [low, low+step .. high] d
+
+haltonSamples = take 1000 $ interleave (halton 2) (halton 3)
+
+-- pkg: normaldistribution
+--
+-- Normal distribution approximation
+-- ---------------------------------
+-- | Box-Muller method for generating two normally distributed
+-- independent random values from two uniformly distributed
+-- independent random values.
+boxMuller :: Floating a => a -> a -> (a,a)
+boxMuller u1 u2 = (r * cos t, r * sin t) where r = sqrt (-2 * log u1)
+                                               t = 2 * pi * u2
+
+-- | Convert a list of uniformly distributed random values into a
+-- list of normally distributed random values. The Box-Muller
+-- algorithms converts values two at a time, so if the input list
+-- has an uneven number of element the last one will be discarded.
+boxMullers :: Floating a => [a] -> [a]
+boxMullers (u1:u2:us) = n1:n2:boxMullers us where (n1,n2) = boxMuller u1 u2
+boxMullers _          = []
+
+-- Halton sequence, a low discrepancy sequence to feed boxMullers
+-- https://pbr-book.org/3ed-2018/Sampling_and_Reconstruction/The_Halton_Sampler
+-- https://en.wikipedia.org/wiki/Low-discrepancy_sequence
+--
+-- Halton sequence for given base
+halton :: Int -> [Double]
+halton b = map (go 1 0) [1..] where
+  go :: Double -> Double -> Int -> Double
+  go f r i
+    | i > 0
+    , let f' = f / (fromIntegral @_ @Double b)
+    , let r' = r + f'*(fromIntegral @_ @Double $ i `mod` b)
+    , let i' = floor (fromIntegral i / fromIntegral b)
+    = go f' r' i'
+    | otherwise
+    = r
+
+interleave xs ys = concat (transpose [xs, ys])
+
+-- flattened R^2 halton sequence of base b1 and b2 (b1 and b2 are coprimes)
+-- halton2 b1 b2 = halton b1 `interleave` halton b2
+
+main = print (4 ~ 20 * 4 ~ 10)
