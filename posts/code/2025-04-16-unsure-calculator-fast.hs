f869062090a6cb45ce9c700706815bc32d180205
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-04-23 19:02:52 +0100

    Exactly 100 lines

--- posts/code/2025-04-16-unsure-calculator-fast.hs
+++ posts/code/2025-04-16-unsure-calculator-fast.hs
@@ -1,64 +1,54 @@
-#!/usr/bin/env cabal
 {- cabal:
 build-depends: base, random, containers
 -}
-{-# LANGUAGE LambdaCase, GADTs, ViewPatterns, GHC2021 #-}
--- TODO: Try using Kahan numerals arithmetic
--- TODO: Take the FT of samples and should be flat
--- TODO: Try random as control
--- TODO: Possible to write in closed form (would look like AD?)
---        -- As a follow up blog post
-import Control.Applicative
-import Control.Monad
+{-# LANGUAGE GADTs, ViewPatterns, GHC2021 #-}
 import Text.Printf
-import Data.Functor.Const
-import qualified Data.Map as M
-import Data.Foldable (minimumBy, maximumBy)
+import Control.Monad (liftM, ap)
 import Data.Function (on)
-import Data.Maybe
-import Data.List (transpose, sort)
-import Data.IORef
-import System.Random -- TODO
-import System.IO.Unsafe
+import Data.List (sort, minimumBy)
+import System.Random
+import qualified Data.Map as M
+
+--- Distribution ---------------------------------------------------------------
 
 -- https://mlg.eng.cam.ac.uk/pub/pdf/SciGhaGor15.pdf
 data Dist a where
   Return :: a -> Dist a
-  Bind :: (Dist b) -> (b -> Dist a) -> Dist a
-  Primitive :: Sampleable d => d a -> Dist a
-  -- Conditional :: (a -> Prob) -> Dist a -> Dist a
+  Bind   :: Dist b -> (b -> Dist a) -> Dist a
+  Normal :: Double -> Double -> Dist Double
 
-instance Functor Dist where
-  fmap = liftM
-instance Applicative Dist where
-  pure = Return; (<*>) = ap
-instance Monad Dist where
-  (>>=) = Bind
+instance Monad       Dist where (>>=) = Bind
+instance Applicative Dist where pure  = Return; (<*>) = ap
+instance Functor     Dist where fmap  = liftM
 
-class Sampleable d where
-  sample :: StdGen -> d a -> a
+sample :: StdGen -> Dist a -> a
+sample g d = case d of
+  Return x -> x
+  Normal mean std_dev -> n1*std_dev + mean
+    where ((u1, u2), _) = uniformR ((0,0), (1,1)) g
+          (n1,  _)      = boxMuller u1 u2
+  Bind d f -> sample g1 (f (sample g2 d))
+    where (g1, g2) = splitGen g
 
-instance Sampleable Dist where
-  sample g = \case
-    Return x -> x
-    Primitive p -> sample g p
-    Bind d f -> sample g1 . f $ sample g2 d
-      where
-        (g1, g2) = splitGen g
+boxMuller u1 u2 = (r * cos t, r * sin t) where r = sqrt (-2 * log u1)
+                                               t = 2 * pi * u2
+
+--- Expressions ----------------------------------------------------------------
 
 data Expr
-  = Num Double | Add Expr Expr | Mul Expr Expr
-  | Abs Expr | Signum Expr | Negate Expr
-  | Div Expr Expr
-  | Exp Expr | Log Expr
-  | Sin Expr | Cos Expr
+  = Num Double
+  | Add Expr Expr   | Mul Expr Expr
+  | Abs Expr        | Signum Expr
+  | Negate Expr     | Div Expr Expr
+  | Exp Expr        | Log Expr
+  | Sin Expr        | Cos Expr
   | Range Expr Expr
 
 (~) = Range
 
 instance Num Expr where
   (+) = Add; (*) = Mul
-  negate = Negate; abs = Abs;
+  negate = Negate; abs = Abs
   signum = Signum; fromInteger = Num . fromInteger
 
 instance Fractional Expr where
@@ -67,11 +57,8 @@ instance Fractional Expr where
 instance Floating Expr where
   pi = Num pi; exp = Exp; log = Log; sin = Sin; cos = Cos
 
-instance Show Expr where
-  show = showSamples . cumulative . collapseIntervals 50 . sample (mkStdGen 0) . sequence . replicate 25000 . eval
-
 eval :: Expr -> Dist Double
-eval = \case
+eval e = case e of
   Num d       -> return d
   Add e1 e2   -> (+) <$> eval e1 <*> eval e2
   Mul e1 e2   -> (*) <$> eval e1 <*> eval e2
@@ -86,71 +73,28 @@ eval = \case
   Range e1 e2 -> do
     a <- eval e1
     b <- eval e2
-    let
-      mean = (a + b) / 2
-      std_dev = (b - a) / 4
-    normal mean std_dev
+    let mean = (a + b) / 2
+        std_dev = (b - a) / 4
+    Normal mean std_dev
 
-data Normal a where
-  Normal :: (StdGen -> Double) -> Normal Double
+--- Drawing --------------------------------------------------------------------
 
-instance Sampleable Normal where
-  sample g (Normal s) = s g
+instance Show Expr where
+  show e = concatMap showRow intervals where
+    samples = take 25000 . sample (mkStdGen 0) . sequence . repeat . eval
+    intervals = collapseIntervals 40 (samples e)
+    line p = replicate spaces ' ' ++ replicate normalized ':'
+      where spaces = 35 - normalized
+            normalized = round ((p/max_prob) * 30)
+            max_prob = maximum $ map snd intervals
+    showRow (elem, prob) = line prob ++ " | " ++ printf "%.1f (%.1f" elem (prob*100) ++ "%)\n"
 
-normal :: Double -> Double -> Dist Double
-normal mean std_dev = Primitive $ Normal $ \g0 ->
-  let (u1, g1)  = uniformR (0, 1) g0
-      (u2,  _)  = uniformR (0, 1) g1
-      (n1, _n2) = boxMuller u1 u2
-      !result   = n1*std_dev + mean
-   in result
-
-boxMuller u1 u2 = (r * cos t, r * sin t) where r = sqrt (-2 * log u1)
-                                               t = 2 * pi * u2
-
--- -- Halton sequence, a low discrepancy sequence to feed boxMullers
--- -- https://en.wikipedia.org/wiki/Low-discrepancy_sequence
--- halton :: Int {-^ Base -} -> [Double]
--- halton b = map (go 1 0) [1..] where
---   go f r i
---     | i > 0
---     , let f' = f / (fromIntegral b)
---     , let r' = r + f'*(fromIntegral $ i `mod` b)
---     , let i' = floor (fromIntegral i / fromIntegral b)
---     = go f' r' i'
---     | otherwise
---     = r
-
---------------------------------------------------------------------------------
--- Drawing
---------------------------------------------------------------------------------
-
-squishP xs = M.toList $ M.fromListWith (+) xs
-normP xs = [(x, p / q) | let q = sum (map snd xs), (x, p) <- xs]
-
-showSamples xs = concatMap showRow xs
-  where
-    max_prob = snd $ maximumBy (compare `on` snd) xs :: Double
-    showRow (elem, prob) = drawline max_prob prob ++ " | " ++ printf "%.1f (%.1f" elem (prob*100) ++ "%)\n"
-
-drawline :: Double -> Double -> String
-drawline max n = replicate spaces ' ' ++ replicate normalized ':' where
-  spaces = 35 - normalized
-  normalized = (round ((n/max) * 30))
-
-intervals boxes dist = do
-  s <- dist
-  return $ fst $ minimumBy (compare `on` snd) $
-    map (\box -> (box, abs (box - s))) boxes
-
-cumulative :: Ord a => [a] -> [(a, Double)]
-cumulative = normP . squishP . map (,1)
-
--- | Simplify samples into N intervals
-collapseIntervals :: Int -> [Double] -> [Double]
+collapseIntervals :: Int -> [Double] -> [(Double, Double)]
 collapseIntervals n (sort -> samples) =
-  let low  = head samples
-      high = last samples
-      step = (high - low) / fromIntegral n
-   in intervals [low-step, low .. high+step] samples
-
+  let (low, high) = (head samples, last samples)
+      step  = (high - low) / fromIntegral n
+      boxes = [low, low+step .. high] 
+      total = fromIntegral (length samples)
+   in M.toList $ M.map (/total) $ M.fromListWith (+)
+        [ (minimumBy (compare `on` \box -> abs (box - s)) boxes, 1)
+        | s <- samples ]
